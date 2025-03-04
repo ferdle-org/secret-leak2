8c47d18a4abe4330cf53817e3b76e8706473d951
Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>
2025-03-04 16:06:59 -0500

    Create testing.py
    Signed-off-by: Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>

---
+++ testing.py
@@ -0,0 +1,8 @@
+#Auth
+AUTH_URL: '',
+AUTH_SCOPES: ['read-listing', 'read-enquiry-reports', 'read-dataset-api'],
+
+SERVICES_AUTH_CLIENT: {
+ID: 'test',
+SECRET: '88156a0f542644908fdc178186a16415',
+},
