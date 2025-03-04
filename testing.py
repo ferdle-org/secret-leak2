91414b92c800f1feb2a2008650fdb656337365cc
Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>
2025-03-04 16:10:43 -0500

    Update testing.py
    Signed-off-by: Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #Auth
-AUTH_URL='',
-AUTH_SCOPES= ['read-listing', 'read-enquiry-reports', 'read-dataset-api'],
+ID='test'
+SECRET='88156a0f542644908fdc178186a16415'
 
-SERVICES_AUTH_CLIENT= {ID: 'test', SECRET: '88156a0f542644908fdc178186a16415'}
+print(SECRET)
