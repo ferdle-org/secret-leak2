165b8e6bafbfa31031630115061a912e05ded95c
Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>
2025-03-05 09:28:06 -0500

    Update teste.py
    Signed-off-by: Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>

--- teste.py
+++ teste.py
@@ -1,8 +1,13 @@
-import pandas
+import requests
 
-curl -L \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/ferdleorg/test/actions/jobs/JOB_ID/rerun
+# GitHub API details
+url = "https://api.github.com/repos/ferdleorg/test/actions/jobs/JOB_ID/rerun"
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": "Bearer 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",
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+response = requests.post(url, headers=headers)
+print(response.status_code)
+print(response.json())
