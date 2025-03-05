3a6220adb3804a073cd238951abb735c02fd308b
Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>
2025-03-05 10:15:18 -0500

    Update teste.py
    Signed-off-by: Fernando Ruiz Diaz <88462636+ferdlestier@users.noreply.github.com>

--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ import requests
 url = "https://api.github.com/repos/ferdleorg/test/actions/jobs/JOB_ID/rerun"
 headers = {
     "Accept": "application/vnd.github+json",
-    "Authorization: Bearer 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",
+    "Authorization": "Bearer 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",
     "X-GitHub-Api-Version": "2022-11-28"
 }
 
